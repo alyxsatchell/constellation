eb3fe2f6ee51b7d3afa97fbfc7985de7f5f36552
KemoshCode <kemosh@live.com>
2023-08-12 15:35:05 -0400

    wip of completely new stencilmap

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod  stencil;
 
 #[cfg(test)]
 mod tests {
-    use crate::{stencil::{Stencil, Transformation}, canvas::{Tile, Color, Canvas, Point}};
+    use crate::{stencil::{Stencil, Translation, StencilMap}, canvas::{Tile, Color, Canvas, Point}};
 
 
     struct TestStencil{
@@ -14,7 +14,7 @@ mod tests {
         fn get_map(&self) -> crate::stencil::StencilMap {
             let tile = Tile::new(Color::new(0, 200, 200, true), 1);
             let origin = Point{x:0, y:0};
-            return vec![Transformation::new(Point{x:0,y:0}, origin, None, tile), Transformation::new(Point{x:0,y:1}, origin, None, tile), Transformation::new(Point{x:1,y:0}, origin, None, tile), Transformation::new(Point{x:1,y:1}, origin, None, tile)]
+            return vec![Translation::new(Point{x:0,y:0}, origin, None, tile), Translation::new(Point{x:0,y:1}, origin, None, tile), Translation::new(Point{x:1,y:0}, origin, None, tile), Translation::new(Point{x:1,y:1}, origin, None, tile)]
             // return vec![(Transformation::new(None, (0,0)), tile), (Transformation::new(None, (0,1)), tile), (Transformation::new(None, (1,0)), tile), (Transformation::new(None, (1,1)), tile)];
         }
     }
@@ -24,5 +24,31 @@ mod tests {
         let test_stencil = TestStencil{};
         canvas.update(test_stencil.get_map());
         canvas.draw();
+    }
+
+    #[test]
+    fn test_animation(){
+        let mut canvas = Canvas::new((10,10), (10,10), Color::new(0,0,0,true));
+        let tile = Tile::new(Color::new(0, 200, 200, true), 1);
+        let mut origin = Point{x:0, y:0};
+        let addition = Point{x:1, y: 0};
+        let test_stencil_map: StencilMap = vec![Translation::new(Point{x:0,y:0}, origin, None, tile), Translation::new(Point{x:0,y:1}, origin, None, tile), Translation::new(Point{x:1,y:0}, origin, None, tile), Translation::new(Point{x:1,y:1}, origin, None, tile)];
+        canvas.update(test_stencil_map);
+        canvas.draw();
+        for j in 0..100000000{
+            let f: f32 = 1.1234251324124;
+            f.sqrt();
+        }
+        for i in 1..5{
+            let new_origin = origin + addition;
+            let test_stencil_map: StencilMap = vec![Translation::new(Point{x:0,y:0}, new_origin, Some(origin), tile), Translation::new(Point{x:0,y:1}, new_origin, Some(origin), tile), Translation::new(Point{x:1,y:0}, new_origin, Some(origin), tile), Translation::new(Point{x:1,y:1}, new_origin, Some(origin), tile)];
+            canvas.update(test_stencil_map);
+            canvas.draw();
+            for j in 0..100000000{
+                let f: f32 = 1.1234251324124;
+                f.sqrt();
+            }
+            origin = new_origin;
+        }
     }
 }
--- src/stencil.rs
+++ src/stencil.rs
@@ -1,34 +1,92 @@
+use std::{collections::HashMap, mem};
+
 use crate::canvas::{Tile, Point};
 
-pub type StencilMap = Vec<Transformation>;
-
 pub trait Stencil{
-    fn get_map(&self) -> StencilMap;
+    fn get_map(&self) -> &StencilMap;
+
+    fn get_map_mut(&mut self) -> &mut StencilMap;
+
+    fn generate_new_map(&self) -> HashMap<Point, Tile>;
+
+    fn merge(&mut self, new_map: HashMap<Point, Tile>){
+        self.get_map_mut().merge(new_map)
+    }
+
+    fn update(&mut self){
+        self.merge(self.generate_new_map());
+    }
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct Transformation{
-    pub origin: Point,
-    pub previous_origin: Option<Point>,
-    pub coord: Point,
-    pub tile: Tile
+pub struct StencilMap{
+    pub addition_map: HashMap<Point, Tile>,
+    pub subtraction_map: Vec<Point>,
+    current_map: HashMap<Point, Tile>
 }
 
-impl Transformation{
-    pub fn new(coord: Point, origin: Point, previous_origin: Option<Point>, tile: Tile) -> Transformation{
-        Transformation { 
-            coord,
-            origin,
-            previous_origin,
-            tile
+impl StencilMap{
+    pub fn merge(&mut self, mut new_map: HashMap<Point, Tile>){
+        let mut addition_map = HashMap::new();
+        let mut subtraction_map = Vec::new();
+        let mut current_map: HashMap<Point, Tile> = mem::replace(&mut self.current_map, HashMap::new());
+        //checks what points of the old map are still relevant
+        for i in current_map{
+            let (point, tile) = i;
+            //checks if the point is going to also be present in the next map
+            //if it is not, then it needs to be in the sub map
+            if !new_map.contains_key(&point){
+                subtraction_map.push(point);
+            }
+            //if it is present, it then checks if the tile has changed to be added to the add map
+            else{
+                let new_tile = new_map.remove(&point).unwrap();
+                if tile != new_tile{
+                    addition_map.insert(point, new_tile);
+                    self.current_map.insert(point, new_tile);
+                }
+                else{
+                    self.current_map.insert(point, tile);
+                }
+            }
         }
-    }
-
-    pub fn translation(&self) -> Point{
-        return self.coord + self.origin
-    }
-
-    pub fn previous_translation(&self) -> Point{
-        return self.coord + self.previous_origin.unwrap()
+        //checks the remaining points that are new to new_map
+        for i in new_map{
+            let (point, tile) = i;
+            addition_map.insert(point, tile);
+            self.current_map.insert(point, tile);
+        }
+        self.addition_map = addition_map;
+        self.subtraction_map = subtraction_map;
     }
 }
+
+// pub struct Transformation{
+//     movement: Point,
+// }
+
+// #[derive(Debug, Clone, Copy)]
+// pub struct Translation{
+//     pub origin: Point,
+//     pub previous_origin: Option<Point>,
+//     pub coord: Point,
+//     pub tile: Tile
+// }
+
+// impl Translation{
+//     pub fn new(coord: Point, origin: Point, previous_origin: Option<Point>, tile: Tile) -> Translation{
+//         Translation { 
+//             coord,
+//             origin,
+//             previous_origin,
+//             tile
+//         }
+//     }
+
+//     pub fn translation(&self) -> Point{
+//         return self.coord + self.origin
+//     }
+
+//     pub fn previous_translation(&self) -> Point{
+//         return self.coord + self.previous_origin.unwrap()
+//     }
+// }
